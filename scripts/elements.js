b19cccbc4830335d15b8f20551db84352ab76a67
MINOR Thomas <mthomas.pro@outlook.fr>
2022-08-22 15:53:48 +0200

    v0.1.0: major changes
    - create elements.js file
    - create function for create recipes cards in DOM
    - import function in main.js

---
+++ scripts/elements.js
@@ -0,0 +1,65 @@
+export const CreateRecipeCard = (recipes) => {
+    //Selectionne le container contiendra les recettes
+    const recipeContainer = document.querySelector(".recipe");
+
+    let recipesInDom = ''; //Déclare une variable vide
+
+    //Boucle qui cherche chaque recette
+    recipes.forEach(recipe => {
+        let ingredientsInDom = '';//Déclare une variable vide
+
+        const { ingredients } = recipe; //Récupère les ingredients de chaque recette
+
+        //Boucle qui cherche chaque ingrédient du tableau d'ingr de chaque recette
+        ingredients.forEach(ingredient => {
+            if (ingredient.quantity) { //Si on trouve quantité dans la tableau
+                if (ingredient.unit && ingredient.quantity) { //Si on trouve quantité et unité dans le tableau
+                    //Affiche l'ingrédient, la quantité et l'unité
+                    ingredientsInDom += `
+                        <li><strong>${ingredient.ingredient}: </strong>${ingredient.quantity} ${ingredient.unit}</li>
+                    `;
+                } else { //Si on trouve juste quantity dans le tableau
+                    //Affiche l'ingrédient et la quantité
+                    ingredientsInDom += `
+                        <li><strong>${ingredient.ingredient}: </strong>${ingredient.quantity}</li>
+                    `;
+                }
+            } else { //Si on trouve ni de quantité ni d'unité dans le tableau
+                //Affiche uniquement l'ingrédient
+                ingredientsInDom += `
+                    <li><strong>${ingredient.ingredient}</strong></li>
+                `;
+            }
+        });
+
+
+
+        //Appel de la variable recipesInDom et ajoute chaque recette dans la var
+        recipesInDom += `
+            <article id="recipe${recipe.id}" class="recipe_card">
+                <div class="recipe_card_image"></div>
+                <div class="recipe_card_infos">
+                    <div class="recipe_card_infos_header">
+                        <h2 class="recipe_card_infos_header_title">${recipe.name}</h2>
+                        <div class="recipe_card_infos_header_time">
+                            <i class="fa-regular fa-clock"></i>
+                            <p>${recipe.time} min</p>
+                        </div>
+                    </div>
+                    <div class="recipe_card_infos_details">
+                        <div class="recipe_card_infos_details_ingredients">
+                            <ul>
+                                ${ingredientsInDom}
+                            </ul>
+                        </div>
+                        <p class="recipe_card_infos_details_instructions">
+                            ${recipe.description}
+                        </p>
+                    </div>
+                </div>
+            </article>
+        `
+    });
+
+    recipeContainer.innerHTML = recipesInDom;
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
 //importation des recettes depuis le dossier data
-import { recipe } from "../data/recipes.js";
+import { recipes } from "../data/recipes.js";
 
+//importation des fonctions pour la création des éléments dans le DOM
+import { CreateRecipeCard } from "./elements.js";
+
+CreateRecipeCard(recipes);
